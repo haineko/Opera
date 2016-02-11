50f86bb7b6b0eecfaefb840f51cac238f445d241
Ann Fadeeva <haineko1512@mail.ru>
2016-02-11 14:35:37 +0300

    сделала страницу контактов и исправила шторы

--- js/script.js
+++ js/script.js
@@ -14,6 +14,8 @@ var curtain = document.querySelector(".curtain-back");
 var enrty = document.querySelector(".entry");
 var dish = document.querySelector(".description__link");
 var descr = document.querySelector(".description");
+//var curtainFooter = document.querySelector(".curtain-front__footer");
+var curtainFooter = document.querySelectorAll(".curtain-footer");
  
 loader.addCompletionListener(function() { 
     
@@ -92,13 +94,16 @@ upCurtain();
 //    console.log("элементы не найдены!");
 //  }
 //}
-
+//debugger;
 function upCurtain() {
   if (curtain) {
     btn.addEventListener('click', function(event) {
       event.preventDefault();
       curtain.classList.add('curtain--hide');
       enrty.classList.add('entry--hide');
+      for (var i = 0; i < curtainFooter.length; i++) {
+        curtainFooter[i].classList.add('curtain-footer--show');
+      }
     });
   } else {
     console.log("занавеса нет!");
@@ -147,77 +152,84 @@ function dishSlider() {
 }
 
 dishSlider();
+/* добавляем карту */
+var myMap = document.getElementById("map");
 
-	function initialize() {     
-		var myLatlng = new google.maps.LatLng(53.8924836,27.5782985);
+function initialize() {
     
-		var myOptions = {
-			zoom: 17,
-			center: myLatlng,
-			mapTypeId: google.maps.MapTypeId.ROADMAP,
-      disableDefaultUI: true,
-      styles: [
-  {
-    "featureType": "landscape.man_made",
-    "elementType": "geometry",
-    "stylers": [
-      {
-        "color": "#b5a57c"
-      }, {
-        "lightness": 39
-      }
-    ]
-  }, {
-    "featureType": "road",
-    "elementType": "geometry.fill",
-    "stylers": [
-      {
-        "color": "#ffffff"
-      }
-    ]
-  }, {
-    "featureType": "poi",
-    "stylers": [
-      {
-        "visibility": "simplified"
-      }
-    ]
-  }, {
-    "featureType": "landscape.natural",
-    "elementType": "geometry.fill",
-    "stylers": [
-      {
-        "color": "#a29061"
-      }
-    ]
-  }, {
-    "featureType": "poi.park",
-    "elementType": "geometry.fill",
-    "stylers": [
-      {
-        "color": "#a29061"
-      }
-    ]
-  }
-],
-			scrollwheel: false
-		};
-		var map = new google.maps.Map(document.getElementById("map"), myOptions); 
-		var map_point = new google.maps.MarkerImage("img/map-point.png", new google.maps.Size(69, 82), new google.maps.Point(0,0), new google.maps.Point(35, 41));	
-		var nerds = new google.maps.LatLng(53.8924836,27.5782985);
-		var marker = new google.maps.Marker({
-			position: nerds,
-			map: map,
-			icon: map_point,
-			title: "Casino Opera",
-			zIndex: 8
-		});
-		google.maps.event.addListener(marker, "click", function() {
-			document.location.href = "index.html";
-		});
-	}
-	google.maps.event.addDomListener(window, "load", initialize);
+		
+      var myLatlng = new google.maps.LatLng(53.8924836,27.5782985);
 
+      var myOptions = {
+        zoom: 17,
+        center: myLatlng,
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        disableDefaultUI: true,
+        styles: [
+    {
+      "featureType": "landscape.man_made",
+      "elementType": "geometry",
+      "stylers": [
+        {
+          "color": "#b5a57c"
+        }, {
+          "lightness": 39
+        }
+      ]
+    }, {
+      "featureType": "road",
+      "elementType": "geometry.fill",
+      "stylers": [
+        {
+          "color": "#ffffff"
+        }
+      ]
+    }, {
+      "featureType": "poi",
+      "stylers": [
+        {
+          "visibility": "simplified"
+        }
+      ]
+    }, {
+      "featureType": "landscape.natural",
+      "elementType": "geometry.fill",
+      "stylers": [
+        {
+          "color": "#a29061"
+        }
+      ]
+    }, {
+      "featureType": "poi.park",
+      "elementType": "geometry.fill",
+      "stylers": [
+        {
+          "color": "#a29061"
+        }
+      ]
+    }
+  ],
+        scrollwheel: false
+      };
+      var map = new google.maps.Map(document.getElementById("map"), myOptions); 
+      var map_point = new google.maps.MarkerImage("img/map-point.png", new google.maps.Size(69, 82), new google.maps.Point(0,0), new google.maps.Point(35, 41));	
+      var nerds = new google.maps.LatLng(53.8924836,27.5782985);
+      var marker = new google.maps.Marker({
+        position: nerds,
+        map: map,
+        icon: map_point,
+        title: "Casino Opera",
+        zIndex: 8
+      });
+      google.maps.event.addListener(marker, "click", function() {
+        document.location.href = "index.html";
+      });
+    
+	}
+
+if (myMap) {
+	google.maps.event.addDomListener(window, "load", initialize);
+} 
 
 //var map;
 //var egglabs = new google.maps.LatLng(50.4504996602356, 30.51102876663208);
