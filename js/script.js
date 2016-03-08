46b9ff90879a2b8e63f1b8206e6ec7cd473d5053
Ann Fadeeva <haineko1512@mail.ru>
2016-03-08 23:50:10 +0300

    изменила бар(убрала подложку), поправила шрфит(архив, привилегии), поправила кнопки(главная и бар)

--- js/script.js
+++ js/script.js
@@ -112,7 +112,7 @@ function dishSlider() {
 function stageSlider() {
     if (document.querySelector(".background--stage")) {
       $('.description-stage').slick({
-//          autoplay: true,
+          autoplay: true,
           dots: false,
           fade: true,
           autoplaySpeed: 9000,
@@ -120,7 +120,7 @@ function stageSlider() {
           arrows: false
       });
       $('.slider__wrap').slick({
-//          autoplay: true,
+          autoplay: true,
           dots: false,
           fade: true,
           autoplaySpeed: 9000,
